1d9d374373a4544df55e2ef8977740f60895006b
CreeplynDarker <creeplyndarker@gmail.com>
2026-06-25 14:43:59 -0500

    c.or instruction implemented

--- design sources/decompressor.v
+++ design sources/decompressor.v
@@ -1,5 +1,5 @@
 // FASE 3: descompresor RVC -> RV32I.
-// Soporta F1-F8 y c.xor (F9). El resto->NOP/illegal.
+// Soporta F1-F9 y c.or (F10). El resto->NOP/illegal.
 module decompressor(input  [15:0]     cinstr,
                     output reg [31:0] instr,    // 32 bits expandida
                     output reg        illegal); // 1 = no soportada (aun)
@@ -54,7 +54,8 @@ module decompressor(input  [15:0]     cinstr,
                    case (cinstr[6:5])
                      2'b00:   instr = rtype(7'b0100000, rs2p, 3'b000, rdp, rdp); // c.sub
                      2'b01:   instr = rtype(7'b0000000, rs2p, 3'b100, rdp, rdp); // FASE 9: c.xor
-                     default: illegal = 1'b1;                                    // Fases 10-11
+                     2'b10:   instr = rtype(7'b0000000, rs2p, 3'b110, rdp, rdp); // FASE 10: c.or
+                     default: illegal = 1'b1;                                    // Fase 11
                    endcase
                  else illegal = 1'b1;
           default: illegal = 1'b1;
